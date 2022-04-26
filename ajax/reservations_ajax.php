bb607ec6370fda2edb194a0f80c3800eaff539bc
mcondewol <mconde@wol.group>
2022-04-26 13:11:02 -0600

    updated 26042022

--- ajax/reservations_ajax.php
+++ ajax/reservations_ajax.php
@@ -11,6 +11,7 @@ include '../core/app/DB/conf.php';
 if(array_key_exists("opcion", $_POST)){
 
     $opcion = $_POST["opcion"];
+    
     $data = array();
         if($opcion == 1){
             $query = "SELECT
@@ -34,6 +35,45 @@ if(array_key_exists("opcion", $_POST)){
                     $acciones = "<a href='index.php?view=reservationhistory&id=".$row['id']."' title='Historial' class='btn btn-success btn-xs'><i class='fa fa-history'></i></a>
                                 <a href='index.php?view=editreservation&id=".$row['id']."' title='Editar' class='btn btn-warning btn-xs'><i class='fa fa-pencil'></i></a>
                                 <a href='index.php?view=delreservation&id=".$row['id']."' class='btn btn-danger btn-xs' rel='tooltip' title='Eliminar' class='btn-simple btn btn-danger btn-xs'><i class='fa fa-remove'></i></a>";
+                    
+                    $oldDate = $row["date"];
+                    $timestamp = strtotime($oldDate); 
+                    $newDate = date("d-m-Y", $timestamp );
+
+                    $oldTime = $row["time"];
+                    $newTime = date('H:i A', strtotime($oldTime));
+                                
+                    $data[] = array(
+                        "area" => $row["category"],
+                        "pacient" => $row["pacient"],
+                        "medic" => $row["medic"],
+                        "fecha" => $newDate,
+                        "hora" => $newTime,
+                        "acciones" => $acciones 
+                    );
+                }     
+            } 
+        }else if($opcion == 2){
+            $reservationId = $_POST["reservationId"];
+            $query = "SELECT
+                r.id AS 'id',
+                CONCAT (p.name, ' ', p.lastname) AS 'pacient',
+                CONCAT (m.name, ' ', m.lastname) AS 'medic',
+                c.name AS 'category',
+                r.`date_at` AS 'date',
+                r.`time_at` AS 'time'
+            FROM
+                reservation r
+                INNER JOIN pacient p
+                ON p.`id` = r.`pacient_id`
+                INNER JOIN medic m
+                ON m.`id` = r.`medic_id`
+                INNER JOIN category c
+                ON c.`id` = m.`category_id`;";
+            $stmt = $dbhost->prepare($query);
+            if($stmt->execute()){
+                while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                   
                     
                     $oldDate = $row["date"];
                     $timestamp = strtotime($oldDate); 
